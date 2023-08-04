a903e60e37ccd64484e2a6d53a9b2cc0f7a1b83d
lolihamburgeri <lorent127100@gmail.com>
2023-08-04 15:08:35 +0300

    appload java file

--- Hello.java
+++ Hello.java
@@ -1,7 +1,7 @@
 public class Hello{ 
        public static void main (String[] args){
 	     for (int i=1;i<=8;i++){
-		  System.out.println("Hello World..."+i);
+		  System.out.println("Hamburger"+i);
 		}
 	}
 }	
